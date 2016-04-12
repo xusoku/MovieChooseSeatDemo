3e75a0a0fdf3a2259e07e25af5c5bf4689375521
davis <286636562@qq.com>
2016-04-12 23:41:22 +0800

    爱的色放

--- app/src/main/java/com/example/xusoku/moviechooseseatdemo/MainActivity.java
+++ app/src/main/java/com/example/xusoku/moviechooseseatdemo/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean select(int column_num, int row_num, boolean paramBoolean) {
-               int [] temp= getRealRowAndColumn(row_num,column_num);
+                int[] temp = getRealRowAndColumn(row_num, column_num);
                 String desc = "您选择了第" + temp[0] + "排" + " 第" + temp[1] + "列";
                 showTextToast(desc.toString());
                 return false;
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean cancelSelect(int column_num, int row_num, boolean paramBoolean) {
-                int [] temp= getRealRowAndColumn(row_num,column_num);
+                int[] temp = getRealRowAndColumn(row_num, column_num);
                 String desc = "您取消了第" + temp[0] + "排" + " 第" + temp[1] + "列";
                 showTextToast(desc.toString());
                 return false;
@@ -97,13 +97,13 @@ public class MainActivity extends AppCompatActivity {
                     mSeat.setLoveInd("0");
                     mSeatList.add(mSeat);
                 }
-            if(i==3){
-                mSeatInfo.setDesc(String.valueOf(0));
-                mSeatInfo.setRow(String.valueOf(-1));
-            }else{
-                mSeatInfo.setDesc(String.valueOf(i + 1));
+//            if(i==3){
+//                mSeatInfo.setDesc(String.valueOf(0));
+//                mSeatInfo.setRow(String.valueOf(-1));
+//            }else{
+                mSeatInfo.setDesc(getDesc(i,3));
                 mSeatInfo.setRow(String.valueOf(i + 1));
-            }
+//            }
 
             mSeatInfo.setSeatList(mSeatList);
             list_seatInfos.add(mSeatInfo);
@@ -113,24 +113,73 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private String getDesc(int row,int setN){
+        int a=0;
+        if(setN!=-1){
+            if(row==setN){
+                a=0;
+                return a+"";
+            }
+            a=0;
+            int tempcount=0;
+            if(list_seatInfos.size()>0)
+                for (int z = 0; z < row; z++) {
+                    if(list_seatInfos.get(z).getDesc().equals("0"))
+                        tempcount++;
+                }
+            a=row-tempcount+1;
+        }
+
+
+        return  ""+a;
+    }
 
     private int [] getRealRowAndColumn(int row,int column){
         int [] a={row+1,column+1};
         int rowSize=list_seat_conditions.size();
         int temp=list_seat_conditions.get(row).get(column).intValue();
-        if(temp==0) {
-            for (int i = 0; i < rowSize; i++) {
-                int oneRowNum = list_seat_conditions.get(i).size();
+//        if(temp==0) {
+//            for (int i = 0; i < rowSize; i++) {
+//                int columnSize = list_seat_conditions.get(row).size();
+//
+//                for (int j = 0; j < columnSize; j++) {
+//                    int temp1 = list_seat_conditions.get(row).get(j).intValue();
+//
+//                    if(column>j){
+//                        int tempcount=0;
+//                        for (int z = 0; z < column; z++) {
+//                            int temp2 = list_seat_conditions.get(row).get(z).intValue();
+//                            if(temp2==0){
+//                                tempcount++;
+//                            }
+//                        }
+//                        a[1]=column-tempcount;
+//                    }else {
+//                        a[1] = column + 1;
+//                    }
+//                }
+//            }
+//        }
 
-                for (int j = 0; j < oneRowNum; j++) {
-                    int temp1 = list_seat_conditions.get(i).get(j).intValue();
 
-                    if (i == row && j == column) {
 
-                    }
-                }
+        //计算行
+        int tempcount=0;
+        for (int z = 0; z < row; z++) {
+            if(list_seatInfos.get(z).getDesc().equals("0"))
+                tempcount++;
+        }
+        a[0]=row-tempcount+1;
+
+        //计算列
+        tempcount=0;
+        for (int z = 0; z < column; z++) {
+            int temp2 = list_seat_conditions.get(row).get(z).intValue();
+            if(temp2==0){
+                tempcount++;
             }
         }
+        a[1]=column-tempcount+1;
         return a;
     }
     private void showTextToast(String msg) {
